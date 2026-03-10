131351478abebcef2a7e7f6c79544317e1f17ac3
Rohit <rohithrajka524@gmail.com>
2026-03-10 17:03:14 +0530

    fix: allow both FRONTEND_URL and localhost in CORS origins

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
   credentials: true,
 }));
 app.use(express.json());
